a98215a1b9e1cb745d63fa96c210df31bb48c7d8
Mrkcosta <marc.costa.tabernero@gmail.com>
2023-08-18 16:49:55 +0200

    CityScene, llista total agents

--- Assets/Classes/Controllers/CityController.cs
+++ Assets/Classes/Controllers/CityController.cs
@@ -4,9 +4,12 @@ using TMPro;
 public class CityController : MonoBehaviour
 {
     public CityDataManager cityDataManager;
+    public AgentManager agentManager;
     public BuildingDataManager buildingDataManager; // Referència afegida
+    
     public TMP_Text cityNameText;
-
+    public TMP_Text agentsCityListText;
+    
     void Start()
     {
         if (cityDataManager.dataItems == null || cityDataManager.dataItems.cities.Count == 0)
@@ -16,6 +19,7 @@ public class CityController : MonoBehaviour
         }
 
         CityData defaultCity = cityDataManager.dataItems.cities[0];
+        agentsCityListText.text = AllAgentsInCityToString();
         DisplayCityData(defaultCity);
     }
 
@@ -52,6 +56,20 @@ public class CityController : MonoBehaviour
 
         Vector3 spawnPosition = new Vector3(position.x, 0, position.y);
         Instantiate(buildingPrefab, spawnPosition, Quaternion.identity);
+    }
+    
+    public string AllAgentsInCityToString()
+    {
+        string result = "Agents:\n";
+        foreach (var agent in agentManager.agents)
+        {
+            CityData agentCity = cityDataManager.dataItems.cities.Find(c => c.cityID == agent.currentCityID);
+            if(agentCity != null)
+                result += agent.agentName + ", a " + agentCity.cityName + "\n";
+        }
+        //Debug.Log("Començant a generar la cadena d'agents. Nombre d'agents: " + agentManager.agents.Count);
+        return result;
+        
     }
 
 }
