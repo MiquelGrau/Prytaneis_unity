556a2ceb825754988d92f5d4bce251a36a46b7d1
Mrkcosta <marc.costa.tabernero@gmail.com>
2024-05-12 15:28:39 +0200

    Building interface, mostrar factors bé

--- Assets/Classes/SceneUI/BuildingInterfaces.cs
+++ Assets/Classes/SceneUI/BuildingInterfaces.cs
@@ -13,6 +13,8 @@ public class BuildingInterfaces : MonoBehaviour
     public GameObject prodBuildingPrefab;
     public GameObject prodFactorPrefab;
     
+    private GameObject activeDetailPanel; // Mantenir una referència al panell de detalls actiu
+
     
     // Funció per actualitzar la graella d'edificis per a una ciutat seleccionada
     public void UpdateBuildingGridForCity(CityData currentCity)
@@ -30,7 +32,11 @@ public class BuildingInterfaces : MonoBehaviour
             newBuildingCell.transform.Find("BBasicTemplate").GetComponent<TMP_Text>().text = DataManager.Instance.GetTemplateNameByID(building.BuildingTemplateID);
 
             // Afegeix un event al clicar per mostrar detalls
-            //newBuildingCell.GetComponent<Button>().onClick.AddListener(() => SetupBuildingUI(building));
+            Button detailButton = newBuildingCell.transform.Find("BBasicButtonDetail").GetComponent<Button>();
+            detailButton.onClick.AddListener(() => {
+                Debug.Log($"Edifici {building.BuildingID} ha estat seleccionat, mostrant detalls");
+                SetupBuildingUI(building);
+            });
         }
     }
 
@@ -39,22 +45,36 @@ public class BuildingInterfaces : MonoBehaviour
     // Funció per aplicar les dades de base de l'edifici. Tot el que es quedarà fixe
     public void SetupBuildingUI(Building building)
     {
-        GameObject detailPanel = Instantiate(detailPanelPrefab, transform);
-        detailPanel.GetComponent<RectTransform>().localPosition = new Vector3(40, -150, 0);  
+        if (activeDetailPanel != null) Destroy(activeDetailPanel);  // Elimina possibles duplicats
+        
+        Transform canvasTransform = GameObject.Find("CanvasCityInfoView").transform;
+        activeDetailPanel = Instantiate(detailPanelPrefab, canvasTransform);
+        RectTransform rectTransform = activeDetailPanel.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = new Vector2(50, -50); // Configura les coordenades respecte a les ancoratges
 
-        detailPanel.transform.Find("BDetailID").GetComponent<TMP_Text>().text = building.BuildingID;
-        detailPanel.transform.Find("BDetailName").GetComponent<TMP_Text>().text = building.BuildingName;
-        detailPanel.transform.Find("BDetailTemplate").GetComponent<TMP_Text>().text = DataManager.Instance.GetTemplateNameByID(building.BuildingTemplateID);
-        detailPanel.transform.Find("BDetailOwner").GetComponent<TMP_Text>().text = building.BuildingOwnerID;
+        activeDetailPanel.transform.Find("BDetailID").GetComponent<TMP_Text>().text = building.BuildingID;
+        activeDetailPanel.transform.Find("BDetailName").GetComponent<TMP_Text>().text = building.BuildingName;
+        activeDetailPanel.transform.Find("BDetailTemplate").GetComponent<TMP_Text>().text = DataManager.Instance.GetTemplateNameByID(building.BuildingTemplateID);
+        activeDetailPanel.transform.Find("BDetailOwner").GetComponent<TMP_Text>().text = building.BuildingOwnerID;
+
+        // Botó de tancar el quadre
+        Button closeButton = activeDetailPanel.transform.Find("BDetailCloseButton").GetComponent<Button>();
+        closeButton.onClick.AddListener(() => Destroy(activeDetailPanel));
+
+        // Troba el panel interior per als prefabs productiu i cívic
+        Transform innerPanel = activeDetailPanel.transform.Find("BDetailInnerPanel");
 
         if (building is CivicBuilding civic)
         {
-            GameObject civicPanel = Instantiate(civicBuildingPrefab, detailPanel.transform);
+            GameObject civicPanel = Instantiate(civicBuildingPrefab, innerPanel);
+            civicPanel.GetComponent<RectTransform>().anchoredPosition = Vector2.zero; 
             civicPanel.transform.Find("BCivicFunction").GetComponent<TMP_Text>().text = civic.Function;
         }
         else if (building is ProductiveBuilding productive)
         {
-            GameObject prodPanel = Instantiate(prodBuildingPrefab, detailPanel.transform);
+            //GameObject prodPanel = Instantiate(prodBuildingPrefab, activeDetailPanel.transform);
+            GameObject prodPanel = Instantiate(prodBuildingPrefab, innerPanel);
+            prodPanel.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;  
             prodPanel.transform.Find("BProdInput").GetComponent<TMP_Text>().text = productive.InputEfficiency.ToString("P2");
             prodPanel.transform.Find("BProdOutput").GetComponent<TMP_Text>().text = productive.OutputEfficiency.ToString("P2");
             prodPanel.transform.Find("BProdCycle").GetComponent<TMP_Text>().text = productive.CycleEfficiency.ToString("P2");
@@ -79,35 +99,19 @@ public class BuildingInterfaces : MonoBehaviour
         foreach(ProductiveFactor factor in building.CurrentFactors)
         {
             GameObject factorPrefab = Instantiate(prodFactorPrefab, factorsPanel);
-            factorPrefab.GetComponent<ProductiveFactorUI>().SetupFactorUI(factor);
+            
+            TMP_Text textComponent = factorPrefab.transform.Find("FactorName").GetComponent<TMP_Text>();
+            if (textComponent != null)
+            {
+                textComponent.text = factor.FactorName;
+            }
+            else
+            {
+                Debug.LogError("FactorName TextMeshProUGUI component is missing or incorrectly named on the factor prefab!");
+            }
+
         }
+        
     }
 }
 
-// You would also need a class to control each ProductiveFactor's UI, something like:
-public class ProductiveFactorUI : MonoBehaviour
-{
-    // UI references
-    public Image factorIcon;
-    public TextMeshProUGUI factorNameText;
-    public Button increaseButton;
-    public Button decreaseButton;
-
-    public void SetupFactorUI(ProductiveFactor factor)
-    {
-        factorNameText.text = factor.FactorName;
-        // Configura altres elements UI aquí
-        increaseButton.onClick.AddListener(() => IncreaseFactor(factor));
-        decreaseButton.onClick.AddListener(() => DecreaseFactor(factor));
-    }
-
-    void IncreaseFactor(ProductiveFactor factor)
-    {
-        // Incrementa factor logic aquí
-    }
-
-    void DecreaseFactor(ProductiveFactor factor)
-    {
-        // Decrementa factor logic aquí
-    }
-}
