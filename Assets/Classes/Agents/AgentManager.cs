cdce87dfa27d5d35e4ff228b196a52d109596c42
Mrkcosta <marc.costa.tabernero@gmail.com>
2024-07-01 00:10:25 +0200

    funcio de AgentManager obsoleta,borrat

--- Assets/Classes/Agents/AgentManager.cs
+++ Assets/Classes/Agents/AgentManager.cs
@@ -12,51 +12,6 @@ public class AgentManager : MonoBehaviour
     public Transform agentListParent;  // Aquest és el contenidor (parent) on vols que apareguin els ítems
     public static int SelectedAgentID { get; private set; } = -1;
 
-    void Start() {
-        //LoadAgents();
-    }
-
-    /* public void LoadAgents()
-    {
-        TextAsset[] jsonFiles = Resources.LoadAll<TextAsset>("DDBB_Agents");
-        if (jsonFiles.Length == 0)
-        {
-            Debug.Log("No s'han trobat fitxers JSON dins de DDBB_Agents.");
-            return;
-        }
-        foreach (TextAsset jsonFile in jsonFiles)
-        {
-            Debug.Log($"Processant fitxer {jsonFile.name}...");
-
-            AgentListString agentListString = JsonUtility.FromJson<AgentListString>(jsonFile.text);
-            foreach (AgentString agentString in agentListString.agents)
-            {
-                Agent agent = new Agent
-                {
-                    agentID = int.Parse(agentString.agentID),
-                    agentName = agentString.agentName,
-                    currentCityID = agentString.currentCityID,
-                    money = int.Parse(agentString.money),
-                    inventoryID = int.Parse(agentString.inventoryID)
-                };
-                agents.Add(agent);
-            }
-        }
-        Debug.Log("Nombre total d'agents carregats: " + agents.Count);
-
-        foreach(Agent agent in agents)
-        {
-            Debug.Log($"Creant agent: {agent.agentName}");
-
-            GameObject agentItemInstance = Instantiate(agentItemPrefab, agentListParent);
-            TextMeshProUGUI agentNameText = agentItemInstance.transform.Find("AgentNameText").GetComponent<TextMeshProUGUI>();
-            Button actionButton = agentItemInstance.transform.Find("ActionButton").GetComponent<Button>();
-            
-            agentNameText.text = agent.agentName;
-            actionButton.onClick.AddListener(() => OnAgentActionButtonClicked(agent));
-        }
-    } */
-
     public void OnAgentActionButtonClicked(Agent agent)
     {
         GameData.Instance.SelectedAgent = agent;
